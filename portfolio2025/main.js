0d216a03920f38156a861083d401e5ca69e62a08
k7wqyv21wfh-arch <k7wq.yv21wfh@gmail.com>
2025-09-22 11:12:00 +0900

    Delete portfolio2025 directory

--- portfolio2025/main.js
+++
@@ -1,9 +0,0 @@
-const topBtn = document.getElementById("topBtn");
-
-window.addEventListener('scroll', () => {
-    if(window.scrollY > 400) {
-        topBtn.classList.add("show");
-    }else{
-        topBtn.classList.remove("show");
-    }
-});
